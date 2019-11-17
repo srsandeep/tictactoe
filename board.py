d9345f78f9af61fa5a07314568dead2a80eff9d3
Sandeep <sr.sandeep@yahoo.com>
2019-11-17 18:51:00 +0000

    Adding StateID and action dimension to q_table with invalid entries dropped from it

--- board.py
+++ board.py
@@ -1,7 +1,16 @@
 import numpy as np
 import math
 import pandas as pd
+import os
+from qtable_states import QTable
+import itertools
 import logging
+
+STATE_TABLE_FILE_NAME = 'all_states.csv'
+SARS_ELEMENT_REWARD_INDEX = 2
+GAME_RESULT_WINNER_REWARD = 1
+GAME_RESULT_LOSER_REWARD = -1
+GAME_RESULT_TIE_REWARD = 0
 
 def generate_patterns_at_pos(row_index=0, column_idx=0, out_dim=0, win_length=0):
     pattern_list = []
@@ -53,15 +62,27 @@ def generate_winning_pattern(order, win_count):
 
 class Board:
 
-    def __init__(self, size, win_count=3):
-        self.board_size = size
+    def __init__(self, board_size: int, win_count: int = 3):
+        self.board_size = board_size
         self.win_count = win_count
         self.winning_patterns = generate_winning_pattern(self.board_size, self.win_count)
         # self.winning_patterns = WINNING_PATTERNS
         self.board_state = np.zeros((self.board_size, self.board_size), dtype=int)
 
+        self.state_table_file_name = str(self.board_size) + '_' + STATE_TABLE_FILE_NAME
+        self.state_table_file_path = os.path.join(os.path.dirname(__file__), self.state_table_file_name)
+        self.state_table_file_exists = os.path.exists(self.state_table_file_path)
+        if not self.state_table_file_exists:
+            qtable = QTable(self.board_size)
+        self.board_state_df = pd.read_csv(self.state_table_file_path)
+
     def reset_board(self):
         self.board_state = np.zeros((self.board_size, self.board_size), dtype=int)
+
+    def get_board_state_id(self):
+        found_indices = np.argwhere((self.board_state_df.values == self.board_state.flatten()).all(axis=1)).flatten().tolist()
+        assert len(found_indices) == 1, f'Found indices {found_indices} for search pattern {self.board_state.flatten()}'
+        return found_indices[0]
 
     def mark_move(self, player_id, row_pos, col_pos):
         ret_status = False
@@ -88,6 +109,9 @@ class Board:
     def get_empty_cell_indices(self):
         logging.debug('Empty Indices: {}'.format([each_row_col[0] * self.board_size + each_row_col[1] for each_row_col in np.argwhere(self.board_state==0)]))
         return [each_row_col[0] * self.board_size + each_row_col[1] for each_row_col in np.argwhere(self.board_state==0)]
+
+    def is_valid_move(self, flat_board_state, select_cell):
+        return True if select_cell in [each_empty_index for each_empty_index in np.argwhere(flat_board_state==0)] else False
 
     def is_game_over(self):
         exit_criteria_satisfied = False
@@ -123,10 +147,11 @@ class Player:
         self.win_count = self.win_count + 1
 
     def make_a_move(self):
+        current_state = self.board_obj.get_board_state_id()
         empty_indices = self.board_obj.get_empty_cell_indices()
         assert empty_indices, 'No empty cells to make a move'
         my_selected_move = np.random.choice(np.array(empty_indices))
-        logging.debug('Player {} selected: {}'.format(self.player_id, my_selected_move))
+        logging.debug('Player {} selected: {} in state: {}'.format(self.player_id, my_selected_move, current_state))
         return my_selected_move
 
 class Game:
@@ -144,22 +169,44 @@ class Game:
         for each_player_id in range(self.num_players):
             self.players.append(Player(each_player_id+1, self.board_inst))
 
+        self.q_table_df = self.board_inst.board_state_df.copy()
+        self.q_table_df['StateID'] = self.q_table_df.index.values
+        q_table_only_df = pd.DataFrame(itertools.product(self.board_inst.board_state_df.index.tolist(), range(self.board_size*self.board_size)), columns=['StateID', 'Action'])
+        self.q_table_df = pd.merge(self.q_table_df, q_table_only_df, on=['StateID'])
+        self.q_table_df['move_validity'] = self.q_table_df.apply(lambda x: self.board_inst.is_valid_move(x.values[:-2],x.values[-1]), axis=1)
+
+        self.q_table_df = self.q_table_df[self.q_table_df['move_validity']]
+        self.q_table_df = self.q_table_df.drop('move_validity', axis=1)
+ 
 
     def play_one_game(self):
         self.board_inst.reset_board()
+        state_action_reward_nstate = [list(), list()]
         current_player = np.random.choice(range(self.num_players))
         game_over = False
+        # Initialize result as a tie
+        reward_value = GAME_RESULT_TIE_REWARD
         while not game_over:
+            current_state = self.board_inst.get_board_state_id()
             sel_row_id, sel_col_id = self.board_inst.mark_a_move(self.players[current_player].player_id, self.players[current_player].make_a_move())
-            logging.info('Player: {} selected row:{} col:{}'.format(self.players[current_player].player_id, sel_row_id, sel_col_id))
+            chosen_action = sel_row_id * self.board_size + sel_col_id
+            next_state = self.board_inst.get_board_state_id()
             game_over, winner_id = self.board_inst.is_game_over()
             if game_over:
                 if winner_id == 0:
-                    logging.info('Game drawn. Board state: {}'.format(self.board_inst.board_state))
+                    logging.debug('Game drawn. Board state: {}'.format(self.board_inst.board_state))
                 else:
+                    reward_value = GAME_RESULT_WINNER_REWARD
                     self.players[current_player].increment_win_count()
-                    logging.info('Winner is player_id:{}. Board state:{}'.format(self.players[current_player].player_id, self.board_inst.board_state))
+                    logging.debug('Winner is player_id:{}. Board state:{}'.format(self.players[current_player].player_id, self.board_inst.board_state))
+            logging.info('Current state: {}, Player: {} selected action: {} gets reward: {} with next state: {}'.format(current_state,self.players[current_player].player_id, chosen_action, reward_value, next_state))
+            state_action_reward_nstate[current_player].append([current_state, chosen_action, reward_value, next_state])
             current_player = (current_player + 1) % self.num_players
+            if reward_value == GAME_RESULT_WINNER_REWARD:
+                state_action_reward_nstate[current_player][-1][SARS_ELEMENT_REWARD_INDEX] = GAME_RESULT_LOSER_REWARD
+
+        logging.info(f'Player 1 SARS: {state_action_reward_nstate[0]}')
+        logging.info(f'Player 2 SARS: {state_action_reward_nstate[1]}')
 
     def play_game_n_time(self, num_games):
         for _ in range(num_games):
@@ -173,6 +220,6 @@ class Game:
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
     game_inst = Game(0, 3, 2, win_count=3)
-    game_inst.play_game_n_time(10)
+    game_inst.play_game_n_time(1)
 
     logging.info('Exiting program')
--- qtable_states.py
+++ qtable_states.py
@@ -1,11 +1,37 @@
 import itertools
+import pandas as pd
+import os
+import logging
 
-df = pd.DataFrame(list(itertools.product([0, 1, 2], repeat=9)), columns=['TopLeft', 'TopMid', 'TopRight', 'MidLeft', 'MidMid', 'MidRight', 'BotLeft', 'BotMid', 'BotRight'])
-l1 = df.columns.tolist().copy()
-df['num1'] = df.apply(lambda x: (x[l1].values==1).sum(), axis=1)
-df['num2'] = df.apply(lambda x: (x[l1].values==2).sum(), axis=1)
-df['diff'] = abs(df['num1']-df['num2'])
-df = df[df['diff']<=1]
-df = df.drop(['num1', 'num2', 'diff'], axis=1)
+STATE_TABLE_FILE_NAME = 'all_states.csv'
 
-df.to_csv('all_states.csv')
+class QTable:
+
+    def __init__(self, board_order: int):
+        self.board_dim = board_order * board_order
+        self.state_table_file_name = str(board_order) + '_' + STATE_TABLE_FILE_NAME
+        self.state_table_file_path = os.path.join(os.path.dirname(__file__), self.state_table_file_name)
+        self.state_table_file_exists = os.path.exists(self.state_table_file_path)
+
+        if not self.state_table_file_exists:
+            logging.info('State table File does not exist')
+            self.generate_state_table()
+
+    def generate_state_table(self):
+
+        df = pd.DataFrame(list(itertools.product([0, 1, 2], repeat=9)), columns=['TopLeft', 'TopMid', 'TopRight', 'MidLeft', 'MidMid', 'MidRight', 'BotLeft', 'BotMid', 'BotRight'])
+        l1 = df.columns.tolist().copy()
+        df['num1'] = df.apply(lambda x: (x[l1].values==1).sum(), axis=1)
+        df['num2'] = df.apply(lambda x: (x[l1].values==2).sum(), axis=1)
+        df['diff'] = abs(df['num1']-df['num2'])
+        df = df[df['diff']<=1]
+        df = df.drop(['num1', 'num2', 'diff'], axis=1)
+        df = df.reset_index().drop('index', axis=1)
+
+        df.to_csv(self.state_table_file_path, index=False)
+        logging.info(f'State table file generated! and has shape: {df.shape}')
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+    qtable = QTable(3)
+    logging.info('Exiting the program')
