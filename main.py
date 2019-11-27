5bbc9bb671909cc06dcfe18e1ebb2f87c1e9a791
Sandeep <sr.sandeep@yahoo.com>
2019-11-27 06:25:53 +0000

    More training and strong player 1

--- main.py
+++ main.py
@@ -22,24 +22,24 @@ if __name__ == "__main__":
 
     player5 = QlearningSARSAPlayer(1, 0)
     player5.update_rl_parameters(alpha=0.7, discount_rate=0.9, initial_q_value=0)
-    # player5 = InteractivePlayer(1)
+    player6 = InteractivePlayer(2)
     # player6 = RandomPlayer(2)
-    player6 = QlearningSARSAPlayer(2, 0)
-    player6.update_rl_parameters(alpha=0.6, discount_rate=0.7, initial_q_value=0)
+    # player6 = QlearningSARSAPlayer(2, 0)
+    # player6.update_rl_parameters(alpha=0.6, discount_rate=0.7, initial_q_value=0)
 
     game_inst.register_players([player5, player6])
     for each_player in game_inst.players:
         each_player.game_prestart_hook()
-    game_inst.play_game_n_time(50, learning_mode=False)
+    game_inst.play_game_n_time(3, learning_mode=False)
 
     logging.info('Exiting program')
 
-# SARSA QLearning - P1: alpha=0.7, discount_rate=0.9, initial_q_value=0.6
-#           - P2: alpha=0.6, discount_rate=0.7, initial_q_value=0
+# SARSA QLearning - P1: alpha=0.7, discount_rate=0.9, initial_q_value=0, epsilon=0.1
+#           - P2: alpha=0.6, discount_rate=0.7, initial_q_value=0, epsilon=0.1
 #           - number of games: 50000
-#           - Life saving move = 0.05. Draw = 0.5
+#           - Life saver = 0, Tie = 0
 
-# SARSA QLearning - P1: alpha=0.7, discount_rate=0.9, initial_q_value=0
-#           - P2: alpha=0.6, discount_rate=0.7, initial_q_value=0
-#           - number of games: 50000
+# SARSA QLearning - P1: alpha=0.7, discount_rate=0.9, initial_q_value=0, epsilon=0.5
+#           - P2: alpha=0.6, discount_rate=0.7, initial_q_value=0, epsilon=0.5
+#           - number of games: 70000
 #           - Life saver = 0, Tie = 0
